5793c8046066f1f5fa040586c0d2fa7702567ab6
Mateus R. Pereira <professor.mateusramos@gmail.com>
2025-06-13 22:01:50 -0300

    feat: implementa filtro de  paciente da tabela

---
+++ js/filtro.js
@@ -0,0 +1,24 @@
+var campoFiltro = document.querySelector("#filtrar-pacientes");
+
+campoFiltro.addEventListener("input", function () {
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if (this.value.length > 0) {
+        for (let i = 0; i < pacientes.length; i++) {
+            const paciente = pacientes[i];
+            var nome = paciente.querySelector(".info-nome").textContent;
+            // expressão regular - regex
+            var expressao = new RegExp(this.value, "i");
+            if (!expressao.test(nome)) {
+                paciente.classList.add("invisivel");
+            } else {
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else {
+        for (let i = 0; i < pacientes.length; i++) {
+            const paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
+    }
+});
